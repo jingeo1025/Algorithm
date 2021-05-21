76dd31d7154abc0805385a448e87016f0386d4db
jingeo1025 <84117608+jingeo1025@users.noreply.github.com>
2021-05-21 10:06:08 +0900

    Create 1068번.cpp

---
+++ CodeUp/1068번.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+  int a;
+
+  scanf("%d", &a);
+
+  if(a<=39)
+  {
+    printf("D");
+  }
+  else if(a<=69)
+  {
+    printf("C");
+  }
+  else if(a<=89)
+  {
+    printf("B");
+  }
+  else if(a<=100)
+  {
+    printf("A");
+  }
+  
+  return 0;
+}
